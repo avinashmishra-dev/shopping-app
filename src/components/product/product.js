c631bd800a77d15e8fc1b4eaacc8f3f6f831f879
Avinash Mishra <avimishra177@gmail.com>
2021-09-08 02:44:38 +0530

    Added cart product listing component

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,16 +1,28 @@
-import { CURRENCIES, PRODUCT_ACTIONS } from '../../helpers/constants';
-import Button from '../button/button';
-import classes from './product.module.css'
+import { CURRENCIES, PRODUCT_ACTIONS } from "../../helpers/constants";
+import Button from "../button/button";
+import classes from "./product.module.css";
 
-const Product = ({product, productManager}) => {
-    const currentCurrency = CURRENCIES.pound;
-    return <div className={classes.product}>
-        <span>{product.name}</span>
-        <div>
-            <span className={classes.product__amount}>{currentCurrency} {product.amount}</span>
-            <Button label="Add" disabled={product.isDisabled} onClickEvent={productManager.bind(this, PRODUCT_ACTIONS.add, product.id)}/>
-        </div>
-    </div>;
-}
+const Product = ({ product, productManager }) => {
+  const currentCurrency = CURRENCIES.pound;
+  return (
+    <div className={`product-ruler ${classes.product}`}>
+      <span>{product.name}</span>
+      <div>
+        <span className={classes.product__amount}>
+          {currentCurrency} {product.amount}
+        </span>
+        <Button
+          label="Add"
+          disabled={product.isDisabled}
+          onClickEvent={productManager.bind(
+            this,
+            PRODUCT_ACTIONS.add,
+            product.id
+          )}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Product;
--- src/components/quantity-manager/quantity-manager.js
+++ src/components/quantity-manager/quantity-manager.js
@@ -1,9 +1,21 @@
+import { CURRENCIES } from "../../helpers/constants";
+import classes from "./quantity-manager.module.css";
+
 const QuantityManager = ({ productMetaInfo, productCartInfo }) => {
   return (
     <div>
-      <span>{productMetaInfo.name}</span>
-      <span>{productMetaInfo.amount}</span>
-      <span>{productCartInfo.quantity}</span>
+      <div className={`product-ruler ${classes.product}`}>
+        <span>{productMetaInfo.name}</span>
+        <span>{productMetaInfo.amount}</span>
+        <span className="pull-right">{productCartInfo.quantity}</span>
+        {/*ABOVE LINE WILL BE REMOVED BY ADD/SUB Component*/}
+      </div>
+      <div className={`pull-right highlight product-ruler`}>
+        Savings {CURRENCIES.pound}?
+      </div>
+      <div className={`pull-right product-ruler`}>
+        Item cost {CURRENCIES.pound}?
+      </div>
     </div>
   );
 };
